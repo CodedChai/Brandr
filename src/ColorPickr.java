df281c4f0c49633dfcc07111f256b2ac5e38fc2a
Connor <guardianx515@gmail.com>
2016-10-15 15:18:36 -0500

    Random Colors in an array list. I'm done.

--- src/ColorPickr.java
+++ src/ColorPickr.java
@@ -1,5 +1,38 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
+
 /**
  * Created by Guard on 10/15/2016.
  */
 public class ColorPickr {
+
+
+    public static ArrayList<Color> GenerateColors_GoldenRatioGradient(int colorCount, Color[] gradient, float saturation, float luminance)
+    {
+        ArrayList<Color> colors = new ArrayList<Color>();
+
+//        float goldenRatioConjugate = 0.618033988749895f;
+//        float currentHue = (float)random.NextDouble();
+
+        Random randy = new Random();
+
+        for (int i = 0; i < colorCount; i++)
+        {
+//            HSL hslColor = new HSL(currentHue, saturation, luminance);
+
+            Color newColor = new Color(randy.nextInt(255), randy.nextInt(255), randy.nextInt(255));
+
+            colors.add(newColor);
+
+//            currentHue += goldenRatioConjugate;
+//            currentHue %= 1.0f;
+
+        }
+
+        return colors;
+    }
+
 }
