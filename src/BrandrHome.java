d54ebcf1223529bdf6c58f1ab155260ef5211953
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-10-15 13:11:49 -0500

    the very basic part.

---
+++ src/BrandrHome.java
@@ -0,0 +1,56 @@
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.awt.GLCanvas;
+
+import java.awt.*;
+
+/**
+ * Created by alvarpq on 10/15/2016.
+ */
+public class BrandrHome extends Frame implements GLEventListener
+{
+    static {
+        GLProfile.getDefault();
+    }
+
+    GLProfile glProfile = null;
+    GLCapabilities glCapabilities = null;
+    GLCanvas glCanvas = null;
+
+    public BrandrHome()
+    {
+        super("Brandr");
+
+    }
+
+    public static void main(String[] args)
+    {
+        new BrandrHome();
+    }
+
+    @Override
+    public void init(GLAutoDrawable glAutoDrawable)
+    {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable glAutoDrawable)
+    {
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int x, int y, int width, int height)
+    {
+
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable glAutoDrawable)
+    {
+
+    }
+}
