953ff1e2ed0d17a57eca2a9d155748c7ea67261a
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-10-15 15:33:52 -0500

    Random Colors in an array list. I'm done.

--- src/CoverDraw.java
+++ src/CoverDraw.java
@@ -1,3 +1,7 @@
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
@@ -14,11 +18,13 @@ public class CoverDraw
 
     int height;
     int width;
+    int namePartition;
 
     public CoverDraw(ArrayList<Color> palette, int height, int width, String name)
     {
+        namePartition = height / 4;
         this.width = width;
-        this.height = height;
+        this.height = height - namePartition;
         this.palette = palette;
         this.name = name;
     }
@@ -31,6 +37,22 @@ public class CoverDraw
     private double[] FindFocalPoint()
     {
         return  new double[] {(randy.nextInt(2) + 1) * height/3,(randy.nextInt(2) + 1) * width/3};
+    }
+
+    public void DrawName(GLAutoDrawable glAutoDrawable)
+    {
+        GL2 gl=glAutoDrawable.getGL().getGL2();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        //Set a color (redish - no other components)
+        gl.glColor3f(0.3f,0.0f,0.0f);
+        //Define a primitive -  A polygon in this case
+        gl.glBegin(GL2.GL_POLYGON);
+            gl.glVertex2i( 100, 20);
+            gl.glVertex2i( 100,460);
+            gl.glVertex2i(540,460);
+            gl.glVertex2i(540, 20);
+        gl.glEnd();
+
     }
 
 
